4246a37b9650cfcc45f7e4b534200b866babd1e9
CleiverMejia <131841452+CleiverMejia@users.noreply.github.com>
2024-01-13 14:35:36 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+const contenido = document.getElementById("Contenido");
+const nombre = document.getElementById("nombre");
+const pass = document.getElementById("contraseña");
+
+contenido.innerHTML += pass.getElementsByClassName;
